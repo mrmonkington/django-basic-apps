28d74968c06c221e53535e80ee0c3871ad9d2424
Pig Monkey <pm@pig-monkey.com>
2011-05-28 19:33:46 -0700

    Creating basic blog settings and a context processor to go with them

---
+++ basic/blog/context_processors.py
@@ -0,0 +1,10 @@
+from basic.blog import settings
+
+def blog_settings(request):
+    """
+    Add blog settings to the context, making them available to templates.
+    """
+    return {
+        'blog_name': settings.BLOG_NAME,
+        'blog_description': settings.BLOG_DESCRIPTION
+    }
---
+++ basic/blog/settings.py
@@ -0,0 +1,13 @@
+"""
+These blog settings should not be edited directly.
+Instead, overwrite them in the main project's setting file.
+"""
+from django.conf import settings
+from django.contrib.sites.models import Site
+
+# The name of the blog.
+# This defaults to the name of the site.
+BLOG_NAME = getattr(settings, 'BLOG_NAME', Site.objects.get_current().name)
+
+# A short description of what the blog is about.
+BLOG_DESCRIPTION = getattr(settings, 'BLOG_DESCRIPTION', 'A basic Django blog')
