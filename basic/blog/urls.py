8adedc67d4c11e6fac92508de9599b0ca0ca1cb1
Pig Monkey <pm@pig-monkey.com>
2011-05-30 13:25:15 -0700

    URLs now use two digit month abbreviation

--- basic/blog/urls.py
+++ basic/blog/urls.py
@@ -2,15 +2,15 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('basic.blog.views',
-    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$',
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$',
         view='post_detail',
         name='blog_detail'
     ),
-    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{1,2})/$',
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{1,2})/$',
         view='post_archive_day',
         name='blog_archive_day'
     ),
-    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/$',
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$',
         view='post_archive_month',
         name='blog_archive_month'
     ),
